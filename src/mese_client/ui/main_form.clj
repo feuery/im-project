25704666c822df187158ba1f275a5900b4ce269a
Feuer <feuer@arch>
2014-03-10 22:14:55 +0200

    The client's update of logged-in-users won't lock the emacs anymore

--- src/mese_client/ui/main_form.clj
+++ src/mese_client/ui/main_form.clj
@@ -4,10 +4,10 @@
             [mese-client.ui.listcontrol :refer [person-listbox]]
             [mese-client.friends :refer [get-current-users]]))
 
+(def user-poll-timespan 10) ;Seconds
+
 (defn people-logged-in [sessionid]
-  (lazy-seq
-   (cons (get-current-users sessionid)
-         (people-logged-in sessionid))))
+  (get-current-users sessionid))
 
 (defn show-mainform [sessionid]
   (let [userseq (atom (people-logged-in sessionid))        
@@ -15,6 +15,17 @@
                      :height 600
                      :on-close :dispose
                      :visible? true
-                     :content  (vertical-panel :items [
-                                                       (scrollable (person-listbox (first @userseq)))]))]
+                     :content  (vertical-panel :items [(scrollable (-> (person-listbox (first @userseq))
+                                                                       (config! :id :users)))]))]
+    (doto (Thread.
+           (fn []
+             (reset! userseq (people-logged-in sessionid))
+             (Thread/sleep (* user-poll-timespan 1000))
+             (if (visible? form)
+               (recur))))
+      .start)
+
+    (add-watch userseq :update-thing (fn [_ _ _ new-val]
+                         (println "Caught new-val: " new-val)
+                         (config! (select form [:#users]) :model new-val)))
     true))
