acd49cda56bc3d39ba6d32895310d3610c7fc159
Feuer <feuer@arch>
2014-03-11 21:14:12 +0200

    Selecting the friend shows the discussion frame

--- src/mese_client/ui/main_form.clj
+++ src/mese_client/ui/main_form.clj
@@ -14,18 +14,36 @@
  (seesaw.cells/default-list-cell-renderer
   (fn [this {:keys [value]}] (.setText this (str (f value)))))))
 
+(defn list-selection [windows sessionid e]
+  (defn new-window []
+    (swap! windows assoc (selection e) (discussion-form sessionid (selection e))))
+  (println "List selection called with " (selection e))
+  (if (contains? @windows (selection e))
+    (invoke-later
+     (println "Window found")
+     (if (visible? (get @windows (selection e)))
+       (doto
+           (get @windows (selection e))
+         (.toFront)
+         (.repaint))
+       (new-window)))
+    (do
+      (println "Window not found")
+      (new-window))))
+                                                                                     
 (defn show-mainform [sessionid]
-  (let [userseq (atom (people-logged-in sessionid))        
+  (let [userseq (atom (people-logged-in sessionid))
+        windows (atom {})
         form (frame :width 800
                      :height 600
                      :on-close :dispose
                      :visible? true
-                     :content  (vertical-panel :items [(scrollable (-> (listbox
-                                                                        :model (first @userseq)
-                                                                        :renderer (string-renderer :username)
-                                                                        :listen [:selection (fn [e]
-                                                                                              (discussion-form sessionid (selection e)))])
-                                                                       (config! :id :users)))]))]
+                     :content  (vertical-panel :items [(scrollable (listbox
+                                                                     :model (first @userseq)
+                                                                     :renderer (string-renderer :username)
+                                                                     :id :users
+                                                                     :listen
+                                                                     [:selection (partial list-selection windows sessionid)]))]))]
     (doto (Thread.
            (fn []
              (reset! userseq (people-logged-in sessionid))
